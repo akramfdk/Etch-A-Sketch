52e1d619c334ed41544de5c51c9267239f2f502f
akramfdk <akramjeet.singh@gmail.com>
2024-05-24 18:41:05 +1000

    simple etch a sketch program using event handlers to listen to key strokes

---
+++ main.py
@@ -0,0 +1,35 @@
+import turtle as t
+
+pen = t.Turtle()
+window = t.Screen()
+
+
+def move_forwards():
+    pen.forward(10)
+
+
+def move_backwards():
+    pen.backward(10)
+
+
+def rotate_counter_clockwise():
+    pen.left(5)
+
+
+def rotate_clockwise():
+    pen.right(5)
+
+
+def clear_screen():
+    window.reset()
+
+
+window.listen()
+window.onkey(fun=move_forwards, key="w")
+window.onkey(fun=move_backwards, key="s")
+window.onkey(fun=rotate_counter_clockwise, key="a")
+window.onkey(fun=rotate_clockwise, key="d")
+window.onkey(fun=clear_screen, key="c")
+
+
+window.exitonclick()
